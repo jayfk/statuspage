c0394a9e7e14538fc667e28e1f51adc0d6ca826f
Jannis Gebauer <ja.geb@me.com>
2016-07-12 11:16:29 +0200

    beef up release, create pip package

---
+++ setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import os
+import io
+import sys
+
+from setuptools import setup
+__version__ = "0.2"
+
+with io.open('README.md', 'r', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+with io.open('CHANGELOG.md', 'r', encoding='utf-8') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'pygithub-redux',
+    'click',
+    'jinja2',
+    'tqdm',
+]
+
+long_description = readme + '\n\n' + history
+
+setup(
+    name='statuspage',
+    version=__version__,
+    description=('A statuspage generator that lets you host your statuspage for free on Github.'),
+    long_description=long_description,
+    author='Jannis Gebauer',
+    author_email='ja.geb@me.com',
+    url='https://github.com/jayfk/statuspage',
+    entry_points='''
+        [console_scripts]
+        statuspage=statuspage:cli
+    ''',
+    py_modules=['statuspage'],
+    include_package_data=True,
+    install_requires=requirements,
+    license='MIT',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+    keywords="statuspage"
+)
