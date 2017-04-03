e9b734cb34c16201b7b8ba2bc1299c23eeb9c575
Jannis Gebauer <ja.geb@me.com>
2017-04-03 15:56:12 +0200

    new release 0.9.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import io
 import sys
 
 from setuptools import setup
-__version__ = "0.8.1"
+__version__ = "0.9.0"
 
 with io.open('README.md', 'r', encoding='utf-8') as readme_file:
     readme = readme_file.read()
--- statuspage/statuspage.py
+++ statuspage/statuspage.py
@@ -15,7 +15,7 @@ from collections import OrderedDict
 import markdown2
 import json
 
-__version__ = "0.8.1"
+__version__ = "0.9.0"
 
 try:
     ROOT = sys._MEIPASS
